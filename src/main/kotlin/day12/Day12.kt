9bf815cd898f08d00bff1e34512ef4fcfda9d05c
Joozd <joozd@ius.org>
2022-12-12 13:30:38 +0100

    benchmarking

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -2,8 +2,6 @@ package day12
 
 import common.Solution
 import common.grids.Coordinate
-import common.grids.CoordinateWithValue
-import common.grids.imaging.GifSequenceBuilder
 import common.grids.pathfinding.AStar
 
 class Day12: Solution(12) {
@@ -11,6 +9,7 @@ class Day12: Solution(12) {
     private lateinit var startPos: Coordinate
     private lateinit var endPos: Coordinate
 
+    // 4.6 ms
     override fun answer1(input: List<String>): Int {
         input.forEachIndexed { y, line ->
             line.forEachIndexed { x, c ->
@@ -25,10 +24,11 @@ class Day12: Solution(12) {
             }
         }
         val route = AStar(mountain[startPos]!!, mountain[endPos]!!)
-        buildGif(mountain.values, route.route!!, "c:\\temp\\2022-12-1.gif")
+        //buildGif(mountain.values, route.route!!, "c:\\temp\\2022-12-1.gif")
         return route.distance!!
     }
 
+    //277 ms
     override fun answer2(input: List<String>): Int {
         val shortestRouteStartPos = mountain.values.filter {
             it.height == 'a'
@@ -36,75 +36,8 @@ class Day12: Solution(12) {
             AStar(it, mountain[endPos]!!).distance ?: Int.MAX_VALUE
         }
         val route = AStar(mountain[shortestRouteStartPos]!!, mountain[endPos]!!)
-        buildGif(mountain.values, route.route!!, "c:\\temp\\2022-12-2.gif")
+        //buildGif(mountain.values, route.route!!, "c:\\temp\\2022-12-2.gif")
         return route.distance!!
 
     }
-
-}
-
-private fun buildGif(mountain: Collection<Position>, route: Collection<Position>, fileName: String){
-    val gifBuilder = GifSequenceBuilder(100, true)
-    gifBuilder.addCoordinates(mountain, scale = 5){
-        if (it.value == 'X')
-            0xFF0000
-        else
-            greenToBrown(getHeightRatio(it.value))
-    }
-
-    for (fraction in 0..route.size step maxOf(1, route.size / 50)){
-        val grid = HashSet(route.take(fraction).map{
-            CoordinateWithValue(it.x, it.y, 'X')
-        })
-        grid.addAll(mountain) // like this so route doesn't get overwritten
-        gifBuilder.addCoordinates(grid, scale = 5){
-            if (it.value == 'X')
-                0xFF0000
-            else
-                greenToBrown(getHeightRatio(it.value))
-        }
-    }
-
-    //add full route
-    val grid = HashSet(route.map{
-        CoordinateWithValue(it.x, it.y, 'X')
-    })
-    grid.addAll(mountain) // like this so route doesn't get overwritten
-    gifBuilder.addCoordinates(grid, scale = 5){
-        if (it.value == 'X')
-            0xFF0000
-        else
-            greenToBrown(getHeightRatio(it.value))
-    }
-
-    gifBuilder.writeGif(fileName)
-
-
-}
-
-private fun getHeightRatio(height: Char): Double =
-    (height - 'a').toDouble() / 25
-
-
-private const val BROWN = 0x964b00
-private const val GREEN = 0x378805
-
-private fun greenToBrown(ratio: Double): Int{
-    val rBrown = BROWN.shr(16)
-    val gBrown = BROWN.shr(8) and 255
-    val bBrown = BROWN and 255
-
-    val rGreen = GREEN.shr(16)
-    val gGreen = GREEN.shr(8) and 255
-    val bGreen = GREEN and 255
-
-    val rDif = rGreen - rBrown
-    val gDif = gGreen - gBrown
-    val bDif = bGreen - bBrown
-
-    val r = (rGreen + rDif * ratio).toInt().shl(16)
-    val g = (gGreen + gDif * ratio).toInt().shl(8)
-    val b = (bGreen + bDif * ratio).toInt()
-
-    return r + g + b
 }
--- src/test/kotlin/TestDay12.kt
+++ src/test/kotlin/TestDay12.kt
@@ -1,5 +1,6 @@
 import day12.Day12
 import org.junit.jupiter.api.Test
+import kotlin.system.measureNanoTime
 import kotlin.test.assertEquals
 
 class TestDay12 {
@@ -9,6 +10,31 @@ class TestDay12 {
     fun test1() {
         assertEquals<Any?>(a1, d.answer1(t1))
         assertEquals<Any?>(b1, d.answer2(t1))
+    }
+
+    @Test
+    fun benchmark1(){
+        val repeats = 100 // 10000
+        measureNanoTime {
+            repeat(repeats){
+                d.answer1()
+            }
+        }.let{
+            println("1: ${String.format("%.3f", it.toDouble()/repeats / 1000000)} ms") // / 1000000 is to go from nanos to millis
+        }
+    }
+
+    @Test
+    fun benchmark2(){
+        d.answer1() // fill caches
+        val repeats = 100 // 10000
+        measureNanoTime {
+            repeat(repeats){
+                d.answer2()
+            }
+        }.let{
+            println("2: ${String.format("%.5f", it.toDouble()/repeats / 1000000)} ms") // / 1000000 is to go from nanos to millis
+        }
     }
 
     private val t1 = """Sabqponm
